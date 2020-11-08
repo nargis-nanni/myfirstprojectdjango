bffb075531a913dc81d96290e7439ccc3f50018b
kajal984 <kajalsexana9@gmail.com>
2020-11-08 16:28:37 +0530

    request method

--- app/views.py
+++ app/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from app.models import Blog
 from app.forms import  BlogForm
+	
 def home(request):
-	form = BlogForm()
 	
+	form = BlogForm()	
 
-	
+	if request.method == 'POST':
+		form = BlogForm(request.POST)
+		if form.is_valid():
+			form.save()
 	all_blog = Blog.objects.all()
 
 	a='angy'
 	k='kajal'
 	x=[a,k]
-	return render(request, 'happy.html', {'form':form})
+	return render(request, 'happy.html', {'form':form,'blogs':all_blog})
 
 
